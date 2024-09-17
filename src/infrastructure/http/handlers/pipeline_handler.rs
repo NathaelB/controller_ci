20cfefca7cedcbaf918a0d949a80696b69b0b7cb
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-09-17 19:31:43 +0200

    chore: Refactor get_pipelines handler to use PipelineService
    This commit modifies the `get_pipelines` handler in the `pipeline_handler.rs` file. It updates the function signature to include a `service` parameter of type `web::Data<dyn PipelineService>`. The handler now uses the `service` to retrieve all pipelines and returns them as a JSON response.
    
    Recent user commits:
    - chore: Refactor PipelineServiceImpl to use Arc for repository
    - feat: Add CommandError enum for handling database errors
    - chore: Add infrastructure modules and services for database and HTTP handling

--- src/infrastructure/http/handlers/pipeline_handler.rs
+++ src/infrastructure/http/handlers/pipeline_handler.rs
@@ -1,6 +1,9 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
+
+use crate::application::ports::pipeline_service::PipelineService;
 
 #[get("/pipelines")]
-pub async fn get_pipelines() -> impl Responder {
-    HttpResponse::Ok().body("get_pipelines")
+pub async fn get_pipelines(service: web::Data<dyn PipelineService>) -> impl Responder {
+    let pipelines = service.find_all().await;
+    HttpResponse::Ok().json(pipelines)
 }
