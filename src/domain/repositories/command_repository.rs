79fe2438f249b08e48166e75a3934abcba6ce534
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-09-19 02:42:44 +0200

    feat: Add find_by_pipeline_id and create methods to ActionRepository
    This commit adds the `find_by_pipeline_id` and `create` methods to the `ActionRepository` trait. The `find_by_pipeline_id` method takes a `pipeline_id` parameter and returns a `Result` containing a vector of `Action` objects or an `ActionError`. The `create` method takes several parameters (`name`, `container_uri`, `type`, `status`) and returns a `Result` containing an `Action` object or an `ActionError`. These methods allow for finding actions by pipeline ID and creating new actions.
    
    Recent user commits:
    - feat: Add action and command service modules
    - feat: Add create_manifest_pipeline method to PipelineService
    - feat: Add ActionService trait for handling actions
    - feat: Add CommandService trait for handling commands
    - feat: Add "name" field to pipelines table
    - chore: Refactor get_pipelines handler to use PipelineService
    - chore: Refactor PipelineServiceImpl to use Arc for repository
    - chore: Refactor action and command repository interfaces
    - feat: Add LogRepository trait for creating logs
    - feat: Add CommandError enum for handling database errors
    - chore: Add infrastructure modules and services for database and HTTP handling
    - chore: Refactor get_pipelines handler to use PipelineService
    - chore: Refactor PipelineServiceImpl to use Arc for repository

--- src/domain/repositories/command_repository.rs
+++ src/domain/repositories/command_repository.rs
@@ -4,6 +4,6 @@ use crate::domain::entities::command::{Command, CommandError};
 
 #[async_trait]
 pub trait CommandRepository: Send + Sync {
-    async fn find_all(&self) -> Result<Vec<Command>, CommandError>;
+    async fn find_by_action_id(&self, action_id: i64) -> Result<Vec<Command>, CommandError>;
     async fn create(&self, action_id: i64, command: String) -> Result<Command, CommandError>;
 }
--- src/domain/repositories/pipeline_repository.rs
+++ src/domain/repositories/pipeline_repository.rs
@@ -3,7 +3,11 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait PipelineRepository: Send + Sync {
-    async fn create(&self, repository_url: &String, name: &String) -> Result<Pipeline, PipelineError>;
+    async fn create(
+        &self,
+        repository_url: String,
+        name: String,
+    ) -> Result<Pipeline, PipelineError>;
     async fn find_all(&self) -> Result<Vec<Pipeline>, PipelineError>;
     async fn find_by_id(&self, pipeline_id: i64) -> Result<Pipeline, PipelineError>;
 }
